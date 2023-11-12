daf9adef35e0d2b2159eb63580d955143a0f30e8
Omar Al-khateeb <134859524+omaryiato@users.noreply.github.com>
2023-11-12 10:27:11 +0200

    Add files via upload

---
+++ Admin/layout/js/admin.js
@@ -0,0 +1,46 @@
+$(function () {
+
+    'use strict';
+
+    // Hide Placeholder On Form Focus
+
+    $('[placeholder]').focus(function () {
+        $(this).attr('data-text', $(this).attr('placeholder'));
+
+        $(this).attr('placeholder', '');
+
+    }).blur(function () {
+        $(this).attr('placeholder', $(this).attr('data-text'));
+    })
+
+
+    // Add Asterisk On Required Field
+
+    // $('input').each(function () {
+    //     if ($(this).attr('required') === 'required') { 
+    //         $(this).after('<span class="asterisk">*</span>');
+    //     }
+    // });
+
+    // Confirmation Message On Button
+
+    $('.confirm').click(function() {
+        return confirm('Are You Sure About Delete This Information !!!');
+    });
+
+    
+
+});
+
+const toggler = document.querySelector(".btn");
+toggler.addEventListener("click",function(){
+    document.querySelector("#sidebar").classList.toggle("collapsed");
+});
+
+
+function restrictInput(event) {
+    const input = event.target;
+    input.value = input.value.replace(/[^0-9]/g, '');
+}
+
+
